7e4d9ae1285c03cf0eb80a9cbf23f4e4918479b1
Morten Knudsen <mok@mortenknudsen.net>
2024-05-28 15:26:35 +0200

    V1

---
+++ EntraGSA_internal_network_Intune_detectionscript.ps1
@@ -0,0 +1,36 @@
+﻿#------------------------------------------------------------------------------------------------
+Write-host "***********************************************************************************************"
+Write-host "Entra Private Access | Intune detection script for internal network detection"
+Write-host ""
+Write-host "Purpose:"
+Write-host "This script will always do Exit 1, as the required checks exists in the remediation script"
+write-host ""
+Write-host "Basically this script will serve as a scheduled task that runs every 1 hour (remediation frequency)"
+Write-host "***********************************************************************************************"
+write-host ""
+#------------------------------------------------------------------------------------------------
+
+##################################
+# VARIABLES
+##################################
+
+    $RegPath                              = "HKLM:\SOFTWARE\EntraGSA_NetworkDetection"
+    $RegKey_LastDetection                 = "EntraGSA_DetectionScript_Last_Run"
+
+
+##################################
+# MAIN PROGRAM
+##################################
+
+    # Create initial reg-path stucture in registry
+        If (-not (Test-Path $RegPath))
+            {
+                $Err = New-Item -Path $RegPath -Force | Out-Null
+            }
+
+    #  Set last run value in registry
+        $Now = (Get-date)
+        $Result = New-ItemProperty -Path $RegPath -Name $RegKey_LastDetection -Value $Now -PropertyType STRING -Force | Out-Null
+
+    # We force Intune detection script to disable to force remediation script to run, where we have the checks
+        Exit 1
---
+++ EntraGSA_suspend_internal_network_Intune_detectionscript.ps1
@@ -0,0 +1,19 @@
+﻿#------------------------------------------------------------------------------------------------
+Write-host "***********************************************************************************************"
+Write-host "Entra Private Access | Suspend Intune remediation script for 'local access detection'"
+Write-host ""
+Write-host "Purpose:"
+Write-host "This script sets key to suspend the remediation script that controls the Entra GSA stop/start behavior"
+Write-host "***********************************************************************************************"
+write-host ""
+#------------------------------------------------------------------------------------------------
+
+    $RegPath                    = "HKLM:\SOFTWARE\EntraGSA_NetworkDetection"
+    $RegKey_SuspendRemediation  = "EntraGSA_SuspendNetworkDetectionRemediation"
+
+    $SuspendStatusValue         = 1   # 0=disable script suspension - 1=enable script suspension
+
+    write-host "Setting the suspend-key to $($SuspendStatusValue)"
+
+    $Result = New-ItemProperty -Path $RegPath -Name $RegKey_SuspendRemediation -Value $SuspendStatusValue -PropertyType DWORD -Force | Out-Null
+
